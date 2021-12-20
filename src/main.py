c3e338546083396f613529a826cd4d5c94ef9e6e
M. Berkay Düzenli <memdo@pm.me>
2021-12-21 01:37:00 +0300

    Resize the image

--- src/main.py
+++ src/main.py
@@ -2,3 +2,11 @@ from PIL import Image
 
 image = Image.open("src/image.jpg")
 image = image.convert("L")
+
+max_height = 30
+ratio = 2.5
+
+x = image.width
+y = image.height
+
+image = image.resize((int(x/y*max_height*ratio), max_height), Image.ANTIALIAS)
