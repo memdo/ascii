099708509261778c0f7f31e9326cc8eca5c7f953
M. Berkay Düzenli <memdo@pm.me>
2021-12-21 02:39:39 +0300

    Better brightness levels for human eye

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from PIL import Image
 
 image = Image.open("src/image.jpg")
-image = image.convert("L")
+image = image.convert("RGB")
 
 
 characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$" 
 max_height = 50
 ratio = 2.5
+items = []
 output = ""
 
 x = image.width
@@ -14,10 +15,13 @@ y = image.height
 
 image = image.resize((int(x/y*max_height*ratio), max_height), Image.ANTIALIAS)
 
+for i in image.getdata():
+    items.append(i)
+
 for h in range(image.height):
     for w in range(image.width):
-        print(w, h)
-        brightness = 255 - image.getpixel((w, h))
+        item = items[h*image.width + w]
+        brightness = int(0.21 * item[0] + 0.72 * item[1] + 0.07 * item[2])
         output += characters[brightness // 4]
     output += "\n"
 
